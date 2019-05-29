7b9f81f77feb2c62550f897b31806bcdc88fde5b
Guma <guma1001@gmail.com>
2019-05-29 18:27:46 +0200

    merge Guma, Gaczorek

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -26,7 +26,7 @@ class HomeScreen extends Component {
                         </Text>
                     </View>
                 </View>
-                <Text>elo</Text>
+                {/* <Text>elo</Text> */}
             </View>
         );
     }
