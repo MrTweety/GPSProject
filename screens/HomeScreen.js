5543400558c6671f9dbc565aa8e59aa0244a441d
Guma <guma1001@gmail.com>
2019-05-29 19:25:36 +0200

    home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground } from 'react-native';
 import {createDrawerNavigator} from 'react-navigation';
 import firebase from 'firebase';
 import LoginScreen from '../screens/LoginScreen'
@@ -11,6 +11,7 @@ class HomeScreen extends Component {
     
     render() {
         return (
+            <ImageBackground source ={require('../assets/road_1.jpg')} style={{width:'100%', height:'100%'}}>
             <View style={{flex:1, flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
                 <View style={{flex: 1, width:'100%', alignItems: 'center', paddingTop: 40, height: 30}}>
                     {Global.user_id ?
@@ -20,14 +21,14 @@ class HomeScreen extends Component {
                     <Text style={{fontSize: 20, fontWeight: 'bold', paddingBottom:20}}>
                     Welcome in Driving Test!
                     </Text>
-                    }<View style ={styles.block}>
-                        <Text style={{textAlign: 'center', padding: 10, fontSize: 16}}>
+                    }<View style={{paddingTop: 40}}>
+                        <Text style={styles.mytext}>
                         Our app is created to share your driving licence exams with others! Before your exam, simply turn on 'recording route' and put your phone into a pocket. After your struggle with examination, you can post your route and help other!
                         </Text>
                     </View>
                 </View>
-                <Text>elo</Text>
             </View>
+            </ImageBackground>
         );
     }
 }
@@ -37,19 +38,14 @@ export default HomeScreen;
 
 const styles = StyleSheet.create({
 
-    block:{
-      padding: 1,
-      width: '90%',
-      height: 150,
-      backgroundColor: '#4ec4ea',
-      borderRadius: 5,
-      alignItems: 'center',
-      justifyContent: 'center',
-      shadowColor: '#000',
-      shadowOffset: {width: 0, height:5 },
-      shadowOpacity: 0.6,
-      shadowRadius: 4,
-      elevation: 5
+    mytext:{
+      textAlign: 'center',
+      padding: 10, 
+      fontSize: 16, 
+      textShadowColor: 'black', 
+      color: 'white',
+      textShadowOffset: {width: 5, height:5 },
+      textShadowRadius: 6,
     }
   
   });
